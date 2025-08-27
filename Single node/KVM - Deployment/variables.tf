698bf4a7c3fff46e851c83db4004e3645b625050
Philippe V <philippe@tapingisnotdead.art>
2025-08-27 15:41:46 +0200

    Added some terraform.tfvars.example files

--- Single node/KVM - Deployment/variables.tf
+++ Single node/KVM - Deployment/variables.tf
@@ -1,57 +1,68 @@
 variable "f5xc_api_url" {
-  type    = string
-  default = "https://<tenant>.console.ves.volterra.io/api"
+  type        = string
+  description = "F5XC API URL for the tenant"
+  default     = "https://<tenant>.console.ves.volterra.io/api"
 }
 
 variable "f5xc_api_p12_file" {
   type        = string
-  description = "F5XC tenant api key"
+  description = "Path to F5XC tenant P12 certificate file"
   default     = "<location of your tenant API key>"
+  sensitive   = true
 }
 
 variable "f5xc_sms_description" {
-  type    = string
-  default = "F5XC KVM site smsv2 site created with Terraform"
+  type        = string
+  description = "Description for the F5XC SMS site"
+  default     = "F5XC KVM site smsv2 site created with Terraform"
 }
 
 variable "f5xc-ce-qcow2" {
+    type        = string
     description = "KVM CE QCOW2 image source"
-    default = "<location of the F5XC CE QCOW2 image"
+    default     = "<location of the F5XC CE QCOW2 image>"
 }
 
 variable "f5xc-ce-memory" {
-    description = "Memory allocated to KVM CE"
-    default = "32000"
+    type        = number
+    description = "Memory allocated to KVM CE in MB"
+    default     = 32000
 }
 
 variable "f5xc-ce-vcpu" {
+    type        = number
     description = "Number of vCPUs allocated to KVM CE"
-    default = "8"
+    default     = 8
 }
 
 variable "f5xc-ce-site-name" {
+    type        = string
     description = "KVM CE site/cluster name"
-    default = "pveys-smsv2-kvm"
+    default     = "pveys-smsv2-kvm"
 }
 
 variable "f5xc-ce-node-name" {
+    type        = string
     description = "KVM CE node name"
-    default = "node-0"
+    default     = "node-0"
 }
 
 variable "f5xc-ce-storage-pool" {
+    type        = string
     description = "KVM CE storage pool name"
-    default = "<your storage pool>"
+    default     = "<your storage pool>"
 }
 
 variable "f5xc-ce-network-slo" {
+    type        = string
     description = "KVM Networking for SLO interface"
-    default = "<your kvm networking for SLO>"
+    default     = "<your kvm networking for SLO>"
 }
 
 variable "f5xc-ce-network-sli" {
+    type        = string
     description = "KVM Networking for SLI interface"
-    default = "<your KVM networking for SLI>"
+    default     = "<your KVM networking for SLI>"
 }
 
 variable "f5xc_software_version" {
